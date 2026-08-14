b36f4adb28bc140c682d64c245fb3f91c4079949
Fateme Davoodian <davoodian.fateme98@gmail.com>
2026-08-15 01:24:24 +0330

    Update script.js

--- script.js
+++ script.js
@@ -4,14 +4,32 @@ const buttons = document.querySelectorAll("button");
 buttons.forEach(function(button) {
   button.addEventListener("click", function() {
     const value = button.innerText;
+
     if (value === "C") {
-      display.value ="";
+    
+      display.value = "";
+    } 
+    else if (value === "⌫") {
+    
+      display.value = display.value.slice(0, -1);
+    } 
+    else if (value === "=") {
+      
+      try {
+      
+        let calculation = display.value.replace(/%/g, "/100");
+        
+       
+        calculation = calculation.replace(/×/g, "*").replace(/÷/g, "/");
+
+        display.value = eval(calculation);
+      } catch (error) {
+        display.value = "Error!";
+      }
+    } 
+    else {
+    
+      display.value += value;
     }
-    else if (value === "="){
-      display.value = eval(display.value);
-  }
-  else{
-    display.value +=value;
-   }
   });
 });
